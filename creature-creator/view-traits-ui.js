0c6cb37de3fed764ea4f0a59d83a9ab082715cae
Andrew <25747328+MrAJGurney@users.noreply.github.com>
2024-10-17 00:07:20 +0100

    fix: include costs in creature action cards
    On the creature creator card costs weren't displaying, as they had been moved to their own property ("prerequisite").
    
    This commit adds a new column where these are now rendered.

--- creature-creator/view-traits-ui.js
+++ creature-creator/view-traits-ui.js
@@ -137,13 +137,14 @@ export class ViewActionsUi extends ViewTraitsUi {
 		return immutableActionIds;
 	}
 
-	addCellsToRow (row, {id, name, effect }) {
+	addCellsToRow (row, {id, name, prerequisite, effect }) {
 		row.insertCell(0).appendChild(document.createTextNode(id));
 		row.insertCell(1).appendChild(document.createTextNode(name));
-		row.insertCell(2).appendChild(document.createTextNode(effect));
+		row.insertCell(2).appendChild(document.createTextNode(prerequisite !== undefined ? prerequisite : '-'));
+		row.insertCell(3).appendChild(document.createTextNode(effect));
 		if (!this.renderOnlyPrintableColumns) {
-			row.insertCell(3).appendChild(this.getDeleteRowButtonForTraitId(id));
-			row.insertCell(4).appendChild(document.createTextNode(
+			row.insertCell(4).appendChild(this.getDeleteRowButtonForTraitId(id));
+			row.insertCell(5).appendChild(document.createTextNode(
 				this.getImmutableTraitIds().includes(id)
 					? Object.values(this.actionAddedByGeneLookupDict).find(({actionId}) => actionId === id).geneName
 					: '-'
